dcb772eba2a7f2f61be5494a391bf2a218680f92
klebersonfialho <klebersonfialhobaleeiro@gmail.com>
2023-05-01 22:15:19 -0300

    feat: Mapeando urls de Treino

--- code/core/apps/Treino/views.py
+++ code/core/apps/Treino/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def criarTreino(request):
+    return HttpResponse("criarTreino")
+
+def editarTreino(request):
+    return HttpResponse("editarTreino")
+
+def deletarTreino(request):
+    return HttpResponse("deletarTreino")
+
+def visualizarTreino(request):
+    return HttpResponse("visualizarTreino")
+
+def listarTreinoPorUsuario(request):
+    return HttpResponse("listarTreinoPorUsuario")
