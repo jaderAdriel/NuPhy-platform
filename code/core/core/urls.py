d1702a9a1138372994cf2dbd646bc43642741c94
klebersonfialho <klebersonfialhobaleeiro@gmail.com>
2023-05-01 22:15:32 -0300

    feat: Mapeando urls

--- code/core/core/urls.py
+++ code/core/core/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("django.contrib.auth.urls")),
     path('accounts/', include("accounts.urls")),
+    path('treino/', include("Treino.urls")),
+    path('prontuario/', include("Prontuario.urls")),
+    path('horario/', include("Horario.urls")),
+    path('consulta/', include("Consulta.urls")),
+    path('dieta/', include("Dieta.urls")),
 ]
