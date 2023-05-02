7d45e93bc740faff483e54692369e3cf0e6d80df
klebersonfialho <klebersonfialhobaleeiro@gmail.com>
2023-05-01 21:47:47 -0300

    Alterando as configurações de email

--- code/core/core/settings.py
+++ code/core/core/settings.py
@@ -145,7 +145,7 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
 # email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'klebersonfialhobaleeiro@gmail.com'
+EMAIL_HOST_PASSWORD = 'modgsapcajynlnpm'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
