4a0729ab64ee2904be3679e1722e7ad9fe6c50a6
klebersonfialho <klebersonfialhobaleeiro@gmail.com>
2023-06-02 20:13:51 -0300

    settings

--- code/core/core/settings.py
+++ code/core/core/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'Prontuario',
     'Treino',
     # 'django_extensions',
+    'bootstrapform',
 ]
 
 # GRAPH_MODELS = {
@@ -138,6 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [ str(BASE_DIR) + "/static"]
 
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL ="/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
