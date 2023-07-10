8b9f396cf413f189c379ff349e99c449bb25fd8f
jader <jaderadriel7@gmail.com>
2023-07-10 15:11:58 -0300

    sidebar itens

--- code/core/apps/Consulta/urls.py
+++ code/core/apps/Consulta/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('criarConsulta/', views.criarConsulta, name='criar_consulta'),
     path('deletarConsulta/<int:consulta_id>/', views.deletarConsulta, name='deletar_consulta'),
     # path('listarConsultaCliente/', views.vizualizarConsulta, name='listar_consulta_cliente'),
-    path('listarConsultasProfissional/', views.listarConsultasProfissional, name='listar_consultas_por_usuario'),
+    path('listarConsultasProfissional/', views.listarConsultasProfissional, name='listar_consultas_por_profissional'),
+    path('listarConsultasCliente/', views.listarConsultasProfissional, name='listar_consultas_por_cliente'),
     path('profissionalGerenciarConsulta/<int:consulta_id>/', views.gerenciarConsulta, name='profissional_gerenciar_consulta'),
-    path('procurar_profissional/', views.pesquisar, name='procurar'),
+    path('procurar_profissional/', views.pesquisar, name='procurar_profissional'),
 ]
--- code/core/apps/accounts/urls.py
+++ code/core/apps/accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('cadastrar', views.cadastrar, name='cadastrar'),
-    path('listar', views.listarUsuariosPendentes, name='listar'),
+    path('listar', views.listarUsuariosPendentes, name='listar_usuarios_pendentes'),
     path('autorizarUsuario/<int:id>/', views.autorizarUsuario, name='autorizarUsuario'),
     path('deletarUsuario/<int:id>/', views.deletarUsuario, name='deletarUsuario'),
     path('', views.profile, name='profile'),
